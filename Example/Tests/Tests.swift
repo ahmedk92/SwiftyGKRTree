0a48c5b1cf03b21ec5334372bab7c96898b3d8dd
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-02-04 23:56:14 +0200

    query tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,27 +2,52 @@ import XCTest
 import SwiftyGKRTree
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func test_NSObjectElementFound() {
+        
+        let expectedElement = "Ahmed"
+        
+        let tree = RTree<String>(maxNumberOfChildren: 10)
+        tree.add(element: "Ali", boundingRect: CGRect(x: 0, y: 0, width: 10, height: 20), splitStrategy: .halve)
+        tree.add(element: expectedElement, boundingRect: CGRect(x: 100, y: 100, width: 10, height: 10), splitStrategy: .halve)
+        
+        XCTAssertEqual(tree.elements(in: CGRect(x: 50, y: 50, width: 100, height: 100)), [expectedElement])
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func test_NSObjectElementNotFound() {
+        
+        let tree = RTree<String>(maxNumberOfChildren: 10)
+        tree.add(element: "Ali", boundingRect: CGRect(x: 0, y: 0, width: 10, height: 20), splitStrategy: .halve)
+        tree.add(element: "Ahmed", boundingRect: CGRect(x: 100, y: 100, width: 10, height: 10), splitStrategy: .halve)
+        
+        XCTAssertEqual(tree.elements(in: CGRect(x: 50, y: 50, width: 100, height: 10)), [])
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    struct MyValue: ExpressibleByIntegerLiteral, Equatable {
+        init(integerLiteral value: Int) {
+            int = value
         }
+        
+        let int: Int
     }
     
+    func test_ElementFound() {
+        
+        let expectedElement: MyValue = 1
+        
+        let tree = RTree<MyValue>(maxNumberOfChildren: 10)
+        tree.add(element: 2, boundingRect: CGRect(x: 0, y: 0, width: 10, height: 20), splitStrategy: .halve)
+        tree.add(element: expectedElement, boundingRect: CGRect(x: 100, y: 100, width: 10, height: 10), splitStrategy: .halve)
+        
+        XCTAssertEqual(tree.elements(in: CGRect(x: 50, y: 50, width: 100, height: 100)), [expectedElement])
+    }
+    
+    func test_ElementNotFound() {
+        
+        let tree = RTree<Int>(maxNumberOfChildren: 10)
+        tree.add(element: 1, boundingRect: CGRect(x: 0, y: 0, width: 10, height: 20), splitStrategy: .halve)
+        tree.add(element: 2, boundingRect: CGRect(x: 100, y: 100, width: 10, height: 10), splitStrategy: .halve)
+        
+        XCTAssertEqual(tree.elements(in: CGRect(x: 50, y: 50, width: 100, height: 10)), [])
+    }
 }
